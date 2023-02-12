9cf678228342afc30be6a12033baca712de2db18
AmeenaFatima01 <ameenafatima059@gmail.com>
2023-02-12 18:15:51 +0500

    working on front end

--- app/src/main/java/com/example/daily_log/MainActivity.java
+++ app/src/main/java/com/example/daily_log/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        etDate=findViewById(R.id.datefld);
+        etSalah=findViewById(R.id.salahfld);
+        etrakat=findViewById(R.id.rakatfld);
+        cbJamat=findViewById(R.id.jamtckbx);
+        cbTahajud=findViewById(R.id.tahajudckbx);
+        btnSubmit=findViewById(R.id.button);
+        btnView=findViewById(R.id.button2);
     }
 }
